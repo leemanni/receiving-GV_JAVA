4d9deb7a94f48d2483ed6e4c414d9b326359e211
leemanni <84111145+leemanni@users.noreply.github.com>
2021-07-22 20:51:26 +0900

    추상클래스. 인터페이스 공부
    인터페이스 -> 추상클래스 -> 클래스 -> 클라이언트 클래스
    인터페이스 : 명시적인 메소드 구현
    추상클래스 : 선택적으로 구현이 필요한 메소드만 구현
    클래스 : 나머지 구현

---
+++ Building.java
@@ -0,0 +1,8 @@
+package exercise;
+
+public interface Building {
+	public void billElectrocity(int money);
+	public void billWater(int mony);
+	public void billGas(int money);
+	float TAX_REFUND_RATIO = 0.3f;
+}
